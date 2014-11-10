069f620c5ed81952462dd4e83ee84acf6200c1db
HerrSteiner <pi@raspberrypi.(none)>
2014-11-10 00:21:34 +0000

    added 3d improvements

--- python/EAMain.py
+++ python/EAMain.py
@@ -23,7 +23,7 @@ camera.setAlpha(64)
 camera.setFrame(100,100,640,480)
 
 # setup 3d
-DISPLAY = pi3d.Display.create(background = (0.0, 0.0, 0.0, 1.))
+DISPLAY = pi3d.Display.create(frames_per_second = 25,background = (0.0, 0.0, 0.0, 1.))
 mylight = pi3d.Light(lightpos=(-2.0, -1.0, 10.0), lightcol=(1.0, 1.0, 0.8), lightamb=(0.25, 0.2, 0.3))
 
 shader = pi3d.Shader("uv_light")
@@ -33,17 +33,28 @@ shader = pi3d.Shader("uv_light")
 #        x=-4, y=2, z=10)
 #mysphere = pi3d.Model(file_string='../../pi3dDemos/models/cow2.obj', name='napf', x=0, y=-1, z=40,
 objects = []
-
+objects.append([])
+objects.append([])
+scene = 0
+scale = 2.
 mysphere = pi3d.Model(file_string='dorn.obj', name='napf', x=0, y=-1, z=-40,
                 sx=2., sy=2., sz=2.)
 mysphere.set_shader(shader)
-objects.append(mysphere)
+objects[0].append(mysphere)
 count = 15
 for i in range(count):
 	clony=mysphere.clone()
 	clony.position(i,-1,-40)
-	objects.append(clony)
+	objects[0].append(clony)
 
+miene  = pi3d.Model(file_string='guteMiene.obj', name='miene', x=0, y=-1, z=-40,
+                sx=2., sy=2., sz=2.)
+miene.set_shader(shader)
+objects[1].append(miene)
+for i in range(count):
+	clony=miene.clone()
+	clony.position(i,-1,-40)
+	objects[1].append(clony)
 #mysphere.reparentTo(anchor)
 #clony.reparentTo(mysphere)
 
@@ -56,6 +67,8 @@ yloc = 1.0;
 rotationX = 0.0
 rotationY = 0.0
 rotationZ = 0.0
+vrcamxloc = 1.0
+vrcamyloc = 1.0
 # OSC Server. there are three different types of server. 
 s = OSC.OSCServer(receive_address) # basic
 ##s = OSC.ThreadingOSCServer(receive_address) # threading
@@ -94,39 +107,61 @@ def printing_handler(addr, tags, stuff, source):
     print "data %s" % stuff
 
 # define a message-handler function for the server to call.
-def control_handler(addr, tags, stuff, source):
-    print "--embeded artists control handleri-"
-    print "received new osc msg %s" % stuff
+def posx_handler(addr, tags, stuff, source):
     global xloc
     xloc = float(stuff[0])
-    #print xloc
-    #print "---x"
-    for o in objects:	
+    for o in objects[0]:	
 		o.position(xloc, yloc, 15.0)
 
-# define a message-handler function for the server to call.
-def fader_handler(addr, tags, stuff, source):
-    print stuff
+    for o in objects[1]:	
+		o.position(xloc, yloc, 15.0)
+
+def posy_handler(addr, tags, stuff, source):
     global yloc
     yloc = float(stuff[0])	
-    #print yloc
-    for o in objects:	
+    for o in objects[0]:	
 		o.position(xloc, yloc, 15.0)
 	
-    #print "---y"
+    for o in objects[1]:	
+		o.position(xloc, yloc, 15.0)
 
+def VrCamPosX_handler(addr, tags, stuff, source):
+    global vrcamxloc
+    vrcamxloc = float(stuff[0])
+    from pi3d.Camera import Camera
+    vrcamera = Camera.instance()
+    if vrcamera:
+    	vrcamera.position((vrcamxloc,vrcamyloc,1.0))
+
+def VrCamPosY_handler(addr, tags, stuff, source):
+    global vrcamyloc
+    vrcamyloc = float(stuff[0])	
+    from pi3d.Camera import Camera
+    vrcamera = Camera.instance()
+    if vrcamera:
+    	vrcamera.position((vrcamxloc,vrcamyloc,1.0))
+	
 def rotationX_handler(addr, tags, stuff, source):
     global rotationX
     rotationX = float(stuff[0])	
     i = 1
-    for o in objects:	
+    for o in objects[0]:	
 	  	o.rotateIncX((rotationX*i)*0.001)
 		i = i + 1
+    i = 1
+    for o in objects[1]:	
+	  	o.rotateIncX((rotationX*i)*0.001)
+		i = i + 1
+
 def rotationY_handler(addr, tags, stuff, source):
     global rotationY
     rotationY = float(stuff[0])
     i = 1;	
-    for o in objects:	
+    for o in objects[0]:	
+	  	o.rotateIncY((rotationY*i)*0.0012)
+		i = i + 1
+    i = 1;	
+    for o in objects[1]:	
 	  	o.rotateIncY((rotationY*i)*0.0012)
 		i = i + 1
 
@@ -134,10 +169,26 @@ def rotationZ_handler(addr, tags, stuff, source):
     global rotationZ
     rotationZ = float(stuff[0])	
     i = 1;	
-    for o in objects:	
+    for o in objects[0]:	
+		o.rotateIncZ((rotationZ*i)*0.00132)
+		i = i + 1
+    i = 1;	
+    for o in objects[1]:	
 		o.rotateIncZ((rotationZ*i)*0.00132)
 		i = i + 1
 
+def scale_handler(addr, tags, stuff, source):
+    global scale
+    scale = float(stuff[0])	
+    for o in objects[0]:	
+		o.scale(scale,scale,scale)
+    for o in objects[1]:	
+		o.scale(scale,scale,scale)
+
+
+def scene_handler(addr, tags, stuff, source):
+    global scene
+    scene = int(stuff[0])	
 
 def cameraAlpha_handler(addr, tags, stuff, source):
     global camera
@@ -234,11 +285,15 @@ def videoSlower_handler(addr, tags, stuff, source):
 
 s.addMsgHandler("/arduino", arduino_handler) # adding our function
 s.addMsgHandler("/print", printing_handler) # adding our function
-s.addMsgHandler("/control/p1", control_handler) # adding our function
-s.addMsgHandler("/fader", fader_handler) # adding our function
-s.addMsgHandler("/rotationX", rotationX_handler) # adding our function
-s.addMsgHandler("/rotationY", rotationY_handler) # adding our function
-s.addMsgHandler("/rotationZ", rotationZ_handler) # adding our function
+s.addMsgHandler("/3d/posX", posx_handler) # adding our function
+s.addMsgHandler("/3d/posY", posy_handler) # adding our function
+s.addMsgHandler("/3d/camera/x", VrCamPosX_handler) # adding our function
+s.addMsgHandler("/3d/camera/y", VrCamPosY_handler) # adding our function
+s.addMsgHandler("/3d/scale", scale_handler) # adding our function
+s.addMsgHandler("/3d/rotationX", rotationX_handler) # adding our function
+s.addMsgHandler("/3d/rotationY", rotationY_handler) # adding our function
+s.addMsgHandler("/3d/scene", scene_handler) # adding our function
+s.addMsgHandler("/3d/rotationZ", rotationZ_handler) # adding our function
 s.addMsgHandler("/camera/alpha", cameraAlpha_handler) # adding our function
 s.addMsgHandler("/camera/effect", cameraEffect_handler) # adding our function
 s.addMsgHandler("/camera/saturation", cameraSaturation_handler) # adding our function
@@ -282,22 +337,15 @@ try :
     while DISPLAY.loop_running():
 	DISPLAY.clear()
 	i = 1
-  	for o in objects:	
-		o.draw()
-	  	o.rotateIncX((rotationX+i)*0.01)
-		o.rotateIncY((rotationY+i)*0.12)
-		o.rotateIncZ((rotationZ-i)*0.02)
-		#o.position(xloc, yloc, 15.0)
-		i = i + 1
+	if (scene < 2): # scene 2 implicit empty 
+  		for o in objects[scene]:	
+			o.draw()
+	  		o.rotateIncX((rotationX+i)*0.01)
+			o.rotateIncY((rotationY+i)*0.12)
+			o.rotateIncZ((rotationZ-i)*0.02)
+			#o.position(xloc, yloc, 15.0)
+			i = i + 1
 	
-  	#clony.draw()
-	#clony.rotateToX(rotationX)
-	#clony.rotateToY(rotationY)
-	#clony.rotateToZ(rotationZ)
-	#clony.position(xloc + 4., yloc, 15.0)
-	#print xloc
-	#print yloc
-
 except KeyboardInterrupt :
     camera.close()
     print "\nClosing OSCServer."
